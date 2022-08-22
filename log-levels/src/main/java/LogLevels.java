3bb24d2848a323fa8271bf900db423ddfcf36f17
djayatillake <dave665@gmail.com>
2022-08-22 22:51:18 +0100

    log levels final

--- log-levels/src/main/java/LogLevels.java
+++ log-levels/src/main/java/LogLevels.java
@@ -1,14 +1,14 @@
 public class LogLevels {
-    
+
     public static String message(String logLine) {
-        throw new UnsupportedOperationException("Please implement the (static) LogLine.message() method");
+        return logLine.split(":")[1].trim();
     }
 
     public static String logLevel(String logLine) {
-        throw new UnsupportedOperationException("Please implement the (static) LogLine.logLevel() method");
+        return logLine.split("]:")[0].trim().toLowerCase().substring(1);
     }
 
     public static String reformat(String logLine) {
-        throw new UnsupportedOperationException("Please implement the (static) LogLine.reformat() method");
+        return message(logLine) + " (" + logLevel(logLine) + ")";
     }
 }

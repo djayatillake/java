e68c65a2c93233e61b7a6d96f625889eb60753f2
djayatillake <dave665@gmail.com>
2022-09-03 23:31:50 +0100

    squeaky clean 1

--- squeaky-clean/src/main/java/SqueakyClean.java
+++ squeaky-clean/src/main/java/SqueakyClean.java
@@ -1,5 +1,23 @@
 class SqueakyClean {
     static String clean(String identifier) {
-        throw new UnsupportedOperationException("Please implement the (static) SqueakyClean.clean() method");
+        StringBuilder cleaned = new StringBuilder();
+        boolean kebab = false;
+        for (char c : identifier.toCharArray()) {
+            if (Character.isISOControl(c)) {
+                cleaned.append("CTRL");
+            } else if (Character.isWhitespace(c)) {
+                cleaned.append('_');
+            } else if (c == '-') {
+                kebab = true;
+            } else if (!Character.isLetter(c) || c >= 'α' && c <= 'ω') {
+                continue;
+            } else if (kebab) {
+                cleaned.append(Character.toUpperCase(c));
+                kebab = false;
+            } else {
+                cleaned.append(c);
+            }            
+        }
+        return cleaned.toString();
     }
 }

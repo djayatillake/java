be9ab17519e870a62f6c4b4230149bdd86654749
djayatillake <dave665@gmail.com>
2022-09-05 21:42:49 +0100

    elons car 1

--- elons-toy-car/src/main/java/ElonsToyCar.java
+++ elons-toy-car/src/main/java/ElonsToyCar.java
@@ -1,17 +1,25 @@
 public class ElonsToyCar {
+    private int battery = 100;
+    private int driven;
     public static ElonsToyCar buy() {
-        throw new UnsupportedOperationException("Please implement the (static) RemoteControlCar.buy()  method");
+        return new ElonsToyCar();
     }
 
     public String distanceDisplay() {
-        throw new UnsupportedOperationException("Please implement the (static) RemoteControlCar.distanceDisplay()  method");
+        return "Driven " + driven + " meters";
     }
 
     public String batteryDisplay() {
-        throw new UnsupportedOperationException("Please implement the (static) RemoteControlCar.batteryDisplay()  method");
+        if (battery > 0) {
+            return "Battery at " + battery + "%";
+        }
+        return "Battery empty";
     }
 
     public void drive() {
-        throw new UnsupportedOperationException("Please implement the (static) RemoteControlCar.drive()  method");
+        if (battery > 0) {
+            driven += 20;
+            battery --;    
+        }    
     }
 }

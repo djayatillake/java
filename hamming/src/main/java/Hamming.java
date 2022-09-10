d178a188e05cfa46cd3340eb1b5b64348a2965dc
djayatillake <dave665@gmail.com>
2022-09-10 23:57:41 +0100

    hamming 1

--- hamming/src/main/java/Hamming.java
+++ hamming/src/main/java/Hamming.java
@@ -1,9 +1,28 @@
 public class Hamming {
+    private String leftStrand;
+    private String rightStrand;
+
     public Hamming(String leftStrand, String rightStrand) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        this.leftStrand = leftStrand;
+        this.rightStrand = rightStrand;
+        if (this.leftStrand.isBlank() && !this.rightStrand.isBlank()) {
+            throw new IllegalArgumentException("left strand must not be empty.");
+        } else if (this.rightStrand.isBlank() && !this.leftStrand.isBlank()) {
+            throw new IllegalArgumentException("right strand must not be empty.");
+        } else if (this.leftStrand.length() != this.rightStrand.length()) {
+            throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");    
+        }
     }
 
+    private int distance;
     public int getHammingDistance() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        char[] l = leftStrand.toCharArray();
+        char[] r = rightStrand.toCharArray();
+        for (int i = 0; i < l.length; i++) {
+            if (l[i]!=r[i]) {
+                distance++;
+            }
+        }
+        return distance;
     }
 }

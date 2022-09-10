dbed75e5d6d513e4dd23d964651f132818ba52fb
djayatillake <dave665@gmail.com>
2022-09-10 22:05:32 +0100

    wizard 1

--- wizards-and-warriors/src/main/java/Fighter.java
+++ wizards-and-warriors/src/main/java/Fighter.java
@@ -1,7 +1,7 @@
 abstract class Fighter {
 
     boolean isVulnerable() {
-        throw new UnsupportedOperationException("Please provide implementation for this method");
+        return false;
     }
 
     abstract int damagePoints(Fighter fighter);
@@ -12,29 +12,35 @@ class Warrior extends Fighter {
 
     @Override
     public String toString() {
-        throw new UnsupportedOperationException("Please implement the toString() method with the required text");
+        return "Fighter is a Warrior";
     }
 
     @Override
     int damagePoints(Fighter wizard) {
-        throw new UnsupportedOperationException("Please implement Warrior.damagePoints() method");
+        return wizard.isVulnerable() ? 10 : 6;
     }
 }
 
 class Wizard extends Fighter {
+    private boolean spell = false;
+
+    @Override
+    public String toString() {
+        return "Fighter is a Wizard";
+    }
 
     @Override
     boolean isVulnerable() {
-        throw new UnsupportedOperationException("Please implement Wizard.isVulnerable() method");
+        return !spell;
     }
 
     @Override
     int damagePoints(Fighter warrior) {
-        throw new UnsupportedOperationException("Please implement Wizard.damagePoints() method");
+        return spell ? 12 : 3;
     }
 
     void prepareSpell() {
-        throw new UnsupportedOperationException("Please implement Wizard.prepareSpell() method");
+        this.spell = true;
     }
 
 }

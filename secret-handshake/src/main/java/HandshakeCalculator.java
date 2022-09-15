212871cfa7a439ac90550a90bd56dfd8e67d1443
djayatillake <dave665@gmail.com>
2022-09-15 13:33:30 +0100

    handshake 1

--- secret-handshake/src/main/java/HandshakeCalculator.java
+++ secret-handshake/src/main/java/HandshakeCalculator.java
@@ -1,9 +1,29 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 class HandshakeCalculator {
 
     List<Signal> calculateHandshake(int number) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        int trim = number % 32;
+        boolean reverse = trim / 16 == 1;
+        trim %= 16;
+        Signal[] signals = new Signal[] {Signal.JUMP, Signal.CLOSE_YOUR_EYES, Signal.DOUBLE_BLINK, Signal.WINK};
+        int denom = 8;
+        List<Signal> signalList = new ArrayList<>();
+        for (int i = 0; i < signals.length; i++) {
+            if (trim / denom == 1) {
+                signalList.add(signals[i]);
+            }
+            trim %= denom;
+            denom /= 2;
+        }
+        if (reverse) {
+            return signalList;
+        } 
+        Collections.reverse(signalList);
+        return signalList;
     }
 
 }

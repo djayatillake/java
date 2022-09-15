7cd3151e58c11fe9410a4f078da4c22dd668e308
djayatillake <dave665@gmail.com>
2022-09-15 15:38:04 +0100

    matrix 1

--- matrix/src/main/java/Matrix.java
+++ matrix/src/main/java/Matrix.java
@@ -1,15 +1,27 @@
+import java.util.Arrays;
 
 class Matrix {
-
+    private int[][] rows;
     Matrix(String matrixAsString) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        rows = new int[matrixAsString.split("\\n").length][matrixAsString.split("\\n")[0].split(" ").length];
+        String[] rawrows = matrixAsString.split("\\n");
+        for (int i = 0; i < rawrows.length; i++) {
+            String[] rawrow = rawrows[i].split(" ");
+            for (int j = 0; j < rawrow.length; j++) {
+                this.rows[i][j] = Integer.parseInt(rawrow[j]);
+            }
+        }
     }
 
     int[] getRow(int rowNumber) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return rows[rowNumber - 1];
     }
 
     int[] getColumn(int columnNumber) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        int[] column = new int[rows.length];
+        for (int i = 0; i < column.length; i++) {
+            column[i] = rows[i][columnNumber-1];
+        }
+        return column;
     }
 }

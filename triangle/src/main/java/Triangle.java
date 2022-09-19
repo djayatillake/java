48587347babdf50849fb10375d30372dbd4fab7b
djayatillake <dave665@gmail.com>
2022-09-19 13:46:42 +0100

    triangle 1

--- triangle/src/main/java/Triangle.java
+++ triangle/src/main/java/Triangle.java
@@ -1,19 +1,27 @@
 class Triangle {
+    private double side1;
+    private double side2;
+    private double side3;
 
     Triangle(double side1, double side2, double side3) throws TriangleException {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        if (side3 >= side1 + side2 || side2 >= side1 + side3 || side1 >= side2 + side3 || side1 <= 0 || side2 <= 0 || side3 <= 0) {
+            throw new TriangleException("Invalid Triangle");
+        }
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
     }
 
     boolean isEquilateral() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return side1 == side2 && side2 == side3;
     }
 
     boolean isIsosceles() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return side1 == side2 || side2 == side3 || side1 == side3;
     }
 
     boolean isScalene() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return side1 != side2 && side2 != side3;
     }
 
 }
--- triangle/src/main/java/TriangleException.java
+++ triangle/src/main/java/TriangleException.java
@@ -1,3 +1,7 @@
 class TriangleException extends Exception {
-
+    public TriangleException (String str)  
+    {  
+        // calling the constructor of parent Exception  
+        super(str);  
+    }
 }

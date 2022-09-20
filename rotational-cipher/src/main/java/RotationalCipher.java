df5abd9806be83727df7843098f6479ce017bd31
djayatillake <dave665@gmail.com>
2022-09-20 13:08:57 +0100

    rotational cipher 1

--- rotational-cipher/src/main/java/RotationalCipher.java
+++ rotational-cipher/src/main/java/RotationalCipher.java
@@ -1,11 +1,22 @@
 class RotationalCipher {
 
+    private int shiftKey;
     RotationalCipher(int shiftKey) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        this.shiftKey = shiftKey;
     }
 
     String rotate(String data) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        char[] carray = data.toCharArray();
+        for (int i = 0; i < carray.length; i++) {
+            if (Character.isAlphabetic(carray[i])) {
+                if (Character.isUpperCase(carray[i])) {
+                    carray[i] = (char)((int)'A' + ((int)carray[i] - (int)'A' + shiftKey) % 26);
+                } else {
+                    carray[i] = (char)((int)'a' + ((int)carray[i] - (int)'a' + shiftKey) % 26);
+                }    
+            }
+        }
+        return String.valueOf(carray);
     }
 
 }

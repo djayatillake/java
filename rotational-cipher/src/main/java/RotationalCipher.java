766db4295fbb726410feb4e0012e22776e171cb9
djayatillake <dave665@gmail.com>
2022-09-20 14:53:57 +0100

    rotational cipher functional

--- rotational-cipher/src/main/java/RotationalCipher.java
+++ rotational-cipher/src/main/java/RotationalCipher.java
@@ -1,27 +1,26 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 class RotationalCipher {
 
     private int shiftKey;
+
     RotationalCipher(int shiftKey) {
         this.shiftKey = shiftKey;
     }
 
-    char RotateChar(char shiftchar, int shiftkey) {
+    char RotateChar(char shiftchar) {
         if (Character.isAlphabetic(shiftchar)) {
             if (Character.isUpperCase(shiftchar)) {
-                return (char)((int)'A' + ((int)shiftchar - (int)'A' + shiftKey) % 26);
+                return (char) ((int) 'A' + ((int) shiftchar - (int) 'A' + shiftKey) % 26);
             } else {
-                return (char)((int)'a' + ((int)shiftchar - (int)'a' + shiftKey) % 26);
-            }    
+                return (char) ((int) 'a' + ((int) shiftchar - (int) 'a' + shiftKey) % 26);
+            }
         }
         return shiftchar;
     }
 
     String rotate(String data) {
-        char[] carray = data.toCharArray();
-        for (int i = 0; i < carray.length; i++) {
-            carray[i] = RotateChar(carray[i], this.shiftKey);
-        }
-        return String.valueOf(carray);
+        return data.chars().mapToObj(i -> this.RotateChar((char) i)).map(String::valueOf).collect(Collectors.joining());
     }
-
 }
